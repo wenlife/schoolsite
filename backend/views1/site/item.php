f4bd016566585135dbdd1e1d55da8e84af14d23a
wenlife207-office <mawl146@qq.com>
2019-10-12 15:59:54 +0800

    delete unused

--- backend/views1/site/item.php
+++
@@ -1,9 +0,0 @@
-<?php
-use yii\helpers\Html;
-use yii\helpers\HtmlPurifier;
-
-  echo '<li class="list-group-item">';
-  echo Html::a($model->headline,['/site/detail',"id"=>$model->infoid]);
-  echo '<span class="pull-right">'.$model->publish_date."</span>";
-  echo "</li>";
-?> 
