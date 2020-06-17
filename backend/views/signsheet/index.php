d8c8542768204dcf154d47e46a80f3202afb95aa
wenlife207-office <mawl146@qq.com>
2020-06-17 16:46:21 +0800

    color

--- backend/views/signsheet/index.php
+++ backend/views/signsheet/index.php
@@ -29,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+           // 'id',
             'name',
             'gender',
             //'old',
